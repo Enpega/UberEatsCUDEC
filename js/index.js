dc22780f5e5f9bc7ca5754d9bde12e6c858adc9e
Enpega <enrique.perez.garibay@gmail.com>
2026-06-10 21:26:19 -0600

    Actualización de datos y agregar platillo

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function mostrarPlatillo(platillo, id) {
   contenido += `
   <div class="card-panel recipe white row" 
-  data-id="${id}">
+  id="${id}">
     <div class="recipe-details">
          <div class="recipe-title">
               ${platillo.nombre}
@@ -20,10 +20,20 @@ function mostrarPlatillo(platillo, id) {
          <div class="recipe-ingredients">
               ${platillo.ingredientes}
          </div>
+         <div class="recipe-price">
+              ${platillo.precio}
+         </div>
     </div>
     <div class="recipe-delete">
          <i class="material-icons" data-id="${id}">delete_outline</i>
     </div>
   </div>`;
   document.querySelector('.recipes').innerHTML = contenido;
+}
+
+function actualizarPlatillo(platillo, id) {
+    let tarjeta = document.getElementById(`${id}`);
+    tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+    tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+    tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
 }
