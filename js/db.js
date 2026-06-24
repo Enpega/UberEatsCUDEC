75c14a33b81b71440578b441c754abd4faa29c76
Enpega <enrique.perez.garibay@gmail.com>
2026-06-23 21:22:39 -0600

    Lista de platillos en pedidos ya muestra información.

--- js/db.js
+++ js/db.js
@@ -1,8 +1,13 @@
+
+
+
+
+
+
 db.collection("platillos").onSnapshot((datos) => {
     datos.docChanges().forEach((registro) => {
         if (registro.type === "added") {
             mostrarPlatillo(registro.doc.data(), registro.doc.id);
-            actualizarALista();
         }
         if (registro.type === "modified") {
             actualizarPlatillo(registro.doc.data(), registro.doc.id);
---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9oN8Dadol1UlSrxfMlepinthO7UnXxRY",
+    authDomain: "ubereatsenrique-7efac.firebaseapp.com",
+    projectId: "ubereatsenrique-7efac",
+    storageBucket: "ubereatsenrique-7efac.firebasestorage.app",
+    messagingSenderId: "908253809839",
+    appId: "1:908253809839:web:cd5dde3d50f636517c985a"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
